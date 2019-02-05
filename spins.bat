288e27dff1633271129a66bca3725bd05e939cbb
peterennisrda <peterennis@resourcedevelopment.net>
2019-02-05 10:27:11 -0800

    Update spins.bat
    echo off, and update description

--- spins.bat
+++ spins.bat
@@ -1,5 +1,7 @@
-\sheerpower\sphandlers\spins_webserver.exe --port 8080
+echo off
+REM Open with e.g.
+REM http://localhost:8080/My_SP4GL_Program.spsrc
 
-REM Open with
-REM http://localhost:8080/test.spsrc
+c:\sheerpower\sphandlers\spins_webserver.exe -port 8080
 
+pause
