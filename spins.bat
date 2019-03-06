85027792b1cea969aaa15d520df8cc30a5cb8e45
Peter <peterennis@resourcedevelopment.net>
2019-03-06 13:34:08 -0800

    fix () set wwwroot for SPINS

--- spins.bat
+++ spins.bat
@@ -2,6 +2,6 @@ echo off
 REM Open with e.g.
 REM http://localhost:8080/My_SP4GL_Program.spsrc
 
-c:\sheerpower\sphandlers\spins_webserver.exe -port 8080
+c:\sheerpower\sphandlers\spins_webserver.exe -port 8080 -wwwroot C:\RDAVSTS\SheerpowerDev\SP_Examples\source
 
 pause
