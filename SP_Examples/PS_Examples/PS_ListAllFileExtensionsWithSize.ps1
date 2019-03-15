757ad9447af4c10b9b8b95d901bc08afba8f271f
Peter <peterennis@resourcedevelopment.net>
2019-03-15 13:09:48 -0700

    Create PS_ListAllFileExtensionsWithSize.ps1

---
+++ SP_Examples/PS_Examples/PS_ListAllFileExtensionsWithSize.ps1
@@ -0,0 +1,22 @@
+﻿# Ref: https://social.technet.microsoft.com/Forums/en-US/814fed50-2d03-45d0-a44d-0fe14a3a855d/list-all-file-extensions-with-size?forum=winserverpowershell
+
+#$dir = "."
+$dir = "C:\Windows"
+
+$count = @{}
+$size = @{}
+gci $dir -recurse |%{
+[int]$count[$_.extension] += 1
+[int64]$size[$_.extension] += $_.length
+}
+$results = @()
+$count.keys | sort |% {
+$result = ""|select extension,extensionLength,count,size
+$result.extension = $_
+$result.extensionLength = "TBD"
+$result.count = $count[$_]
+$result.size = $size[$_]
+$results += $result
+}
+$results | ft -auto
+$results | export-csv c:\temp\result.csv
