0e6ca580f4c11542845731d484e53d7cbb67d52d
Peter <peterennis@resourcedevelopment.net>
2019-03-15 15:10:23 -0700

    chore (format) shift-alt-f VSCode PS extension

--- SP_Examples/PS_Examples/PS_ListAllFileExtensionsWithSize.ps1
+++ SP_Examples/PS_Examples/PS_ListAllFileExtensionsWithSize.ps1
@@ -5,18 +5,18 @@ $dir = "C:\Windows"
 
 $count = @{}
 $size = @{}
-Get-ChildItem $dir -recurse | ForEach-Object{
-[int]$count[$_.extension] += 1
-[int64]$size[$_.extension] += $_.length
+Get-ChildItem $dir -recurse | ForEach-Object {
+    [int]$count[$_.extension] += 1
+    [int64]$size[$_.extension] += $_.length
 }
 $results = @()
 $count.keys | ForEach-Object {
-$result = ""| Select-Object extension,extensionLength,count,size
-$result.extension = $_
-$result.extensionLength = $_.lengths
-$result.count = $count[$_]
-$result.size = $size[$_]
-$results += $result
+    $result = "" | Select-Object extension, extensionLength, count, size
+    $result.extension = $_
+    $result.extensionLength = $_.lengths
+    $result.count = $count[$_]
+    $result.size = $size[$_]
+    $results += $result
 }
 $results | Sort-Object -Property extensionLength | Format-Table -auto
 $results | export-csv c:\temp\result.csv
