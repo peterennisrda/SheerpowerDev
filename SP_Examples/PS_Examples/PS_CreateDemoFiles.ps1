65ad8650ad1301149b2b54f5761b4a7ab3e7ee6d
Peter <peterennis@resourcedevelopment.net>
2019-03-15 15:15:41 -0700

    chore (format) shift-alt-f

--- SP_Examples/PS_Examples/PS_CreateDemoFiles.ps1
+++ SP_Examples/PS_Examples/PS_CreateDemoFiles.ps1
@@ -1,47 +1,43 @@
 ﻿# Ref: https://blogs.technet.microsoft.com/heyscriptingguy/2018/09/15/using-powershell-to-create-a-folder-of-demo-data/
 
 # Provide Folder name and create it
-$Folder='C:\SP_Demo_Data'
+$Folder = 'C:\SP_Demo_Data'
 
 New-Item -ItemType Directory -Path $Folder
 
 # Create a series of 10 files
-for ($x=0;$x -lt 10; $x++)
+for ($x = 0; $x -lt 10; $x++) {
+    # Let's create a completely random filename
+    $filename = "$($Folder)\Logfile$((Get-Random 100000).tostring()).txt"
 
-{
-# Let's create a completely random filename
-$filename="$($Folder)\Logfile$((Get-Random 100000).tostring()).txt"
+    # Now we'll create the file with some content
+    # We're going to build files up to 1K in size
 
-# Now we'll create the file with some content
-# We're going to build files up to 1K in size
+    $limit = (Get-random 1024)
 
-   $limit=(Get-random 1024)
+    # Let's build the random content
+    for ($y = 0; $y -lt $limit; $y++) {
 
-   # Let's build the random content
-   for($y=0;$y -lt $limit;$y++)
-   {
+        # We're building a content of pure ASCII data
+        $a = $a + [char][byte]((Get-Random 64) + 32)
+    }
 
-       # We're building a content of pure ASCII data
-       $a=$a+[char][byte]((Get-Random 64)+32)
+    $DaysToMove = ((Get-Random 120) - 60)
+    $HoursToMove = ((Get-Random 48) - 24)
+    $MinutesToMove = ((Get-Random 120) - 60)
+    $TimeSpan = New-TimeSpan -Days $DaysToMove -Hours $HoursToMove -Minutes $MinutesToMove
 
-   }
+    # Now we adjust the Date and Time by the new TimeSpan
+    # Needs Admin rights to do this as well!
 
-   $DaysToMove=((Get-Random 120) -60)
-   $HoursToMove=((Get-Random 48) -24)
-   $MinutesToMove=((Get-Random 120) -60)
-   $TimeSpan=New-TimeSpan -Days $DaysToMove -Hours $HoursToMove -Minutes $MinutesToMove
+    Set-Date -Adjust $Timespan | Out-Null
 
-   # Now we adjust the Date and Time by the new TimeSpan
-   # Needs Admin rights to do this as well!
+    # Create the file
+    Add-Content -Value $a -Path $filename
 
-   Set-Date -Adjust $Timespan | Out-Null
+    # Now we REVERSE the Timespan by the exact same amount
+    $TimeSpan = New-TimeSpan -Days (-$DaysToMove) -Hours (-$HoursToMove) -Minutes (-$MinutesToMove)
 
-   # Create the file
-   Add-Content -Value $a -Path $filename
-
-   # Now we REVERSE the Timespan by the exact same amount
-   $TimeSpan=New-TimeSpan -Days (-$DaysToMove) -Hours (-$HoursToMove) -Minutes (-$MinutesToMove)
-
-   Set-Date -Adjust ($Timespan) | Out-Null
+    Set-Date -Adjust ($Timespan) | Out-Null
 
 }
