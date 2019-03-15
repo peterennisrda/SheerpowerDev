7b89351e046de9727ab07fd0d9f6f52ab420049a
Peter <peterennis@resourcedevelopment.net>
2019-03-15 12:00:00 -0700

    Create PS_MyFileSystem.ps1
    Initial PS script to get file system info for import to Sheerpower cluster

---
+++ SP_Examples/PS_Examples/PS_MyFileSystem.ps1
@@ -0,0 +1,15 @@
+﻿# Run the following command first in the console to bypass not digitally signed error
+# Ref: http://tritoneco.com/2014/02/21/fix-for-powershell-script-not-digitally-signed/
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+$outputfile = "C:\TEMP\My File System Report.csv"
+$mydir = "."
+
+# Get-ChildItem          ' alias is gci
+# Get-Alias | More       ' show paged list of all aliases
+#
+# Show all aliases for Get-ChildItem
+# Ref: https://stackoverflow.com/questions/6635059/filter-the-output-of-a-command-as-if-it-was-text
+# (Get-Alias | Out-String) -split "`r`n" | Select-String Get-ChildItem
+
+gci $mydir -Recurse | select Mode,Name,FullName,Directory,Length,CreationTime | Format-Table
