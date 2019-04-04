066821e3f530383a1a419d2f9d42374c5e56a391
Peter <peterennis@resourcedevelopment.net>
2019-04-03 17:30:42 -0700

    feat () howto js topnav responsive

--- SP_Examples/wwwroot/index.js
+++ SP_Examples/wwwroot/index.js
@@ -41,3 +41,15 @@ const setNewPosition = () => {
 };
 
 btn.addEventListener("click", setNewPosition);
+
+/**********************************************************************/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
