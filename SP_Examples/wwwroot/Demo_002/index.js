79ef4232d2bf031a0013707c0f371753581ed066
peterennis <peterennis@yahoo.com>
2019-04-08 20:25:39 -0700

    feat () rainbow morph demo

---
+++ SP_Examples/wwwroot/Demo_002/index.js
@@ -0,0 +1,11 @@
+// Ref: https://codepen.io/kathykato/pen/rwNqjP*//
+
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
