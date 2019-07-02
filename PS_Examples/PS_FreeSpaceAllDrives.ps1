d3473e8df6957b8b84e5af38b8e604efb93a45b9
peterennisrda <peterennis@resourcedevelopment.net>
2019-07-02 14:39:19 -0700

    Create PS_FreeSpaceAllDrives.ps1

---
+++ PS_Examples/PS_FreeSpaceAllDrives.ps1
@@ -0,0 +1,1 @@
+Get-PSDrive | Where-Object {$_.Free -gt 1} | ForEach-Object { $Count = 0; Write-Host ""; }{ $_.Name + ": Used :" + "{0:N2}" -f ($_.Used/1Gb) + " Free: " + "{0:N2}" -f ($_.Free/1Gb) + " Total: " + "{0:N2}" -f (($_.Used/1Gb) + ($_.Free/1Gb)); $Count = $Count + $_.Free; }{ Write-Host ""; Write-Host "Total Free Space " ("{0:N2}" -f ($Count/1Gb)) -backgroundcolor magenta }
