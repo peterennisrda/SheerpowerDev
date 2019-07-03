bd30008c404dfebfe461f86d57d755d7a737afd6
peterennisrda <peterennis@resourcedevelopment.net>
2019-07-03 15:53:47 -0700

    Create PS_Acl_Scripting.ps1

---
+++ PS_Examples/PS_Acl_Scripting.ps1
@@ -0,0 +1,15 @@
+# Ref: http://blogs.technet.com/b/heyscriptingguy/archive/2011/11/13/use-powershell-to-quickly-find-installed-software.aspx
+
+# Get-ExecutionPolicy 
+# The command shows the current script execution policy
+# Ref: http://technet.microsoft.com/library/hh847748.aspx
+# Ref: http://technet.microsoft.com/en-us/library/hh849812.aspx
+# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+# The command uses the Set-ExecutionPolicy cmdlet to set an execution policy of Unrestricted for the current user.
+
+Clear-Host
+
+Write-Host "Start=>"
+
+# Ref: https://www.petri.com/how-to-get-ntfs-file-permissions-using-powershell
+Get-Acl -Path W:\peterennis | Format-Table -Wrap
