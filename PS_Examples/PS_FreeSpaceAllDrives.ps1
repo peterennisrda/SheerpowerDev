08fcc526ce70db03cf7e84bcd4a12e8e720507ca
peterenisrda <peterennis@resourcedevelopment.net>
2019-07-11 15:25:35 -0700

    chore(): update output to show operation process of ForEach

--- PS_Examples/PS_FreeSpaceAllDrives.ps1
+++ PS_Examples/PS_FreeSpaceAllDrives.ps1
@@ -1,7 +1,9 @@
+# Ref: https://www.youtube.com/watch?v=f9xPJXslVWE
 Get-PSDrive | Where-Object { $_.Free -gt 1 } | `
-    ForEach-Object { $Count = 0; Write-Host ""; } { $_.Name + ": Used :" + "{0:N2}" -f ($_.Used / 1Gb) + `
+    ForEach-Object { $Count = 0; Write-Host "This is output once at the start"; } { $_.Name + ": Used :" + "{0:N2}" -f ($_.Used / 1Gb) + `
         " Free: " + "{0:N2}" -f ($_.Free / 1Gb) + " Total: " + "{0:N2}" -f (($_.Used / 1Gb) + ($_.Free / 1Gb)); `
-        $Count = $Count + $_.Free; } { Write-Host ""; Write-Host "Total Free Space " ("{0:N2}" -f ($Count / 1Gb)) -BackgroundColor Magenta }
+        $Count = $Count + $_.Free; } { Write-Host ""; Write-Host "This is written once at the end - " `
+        "Total Free Space " ("{0:N2}" -f ($Count / 1Gb)) -BackgroundColor Magenta }
 
 
 Get-Volume
