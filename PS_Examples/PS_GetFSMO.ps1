e4df5c10c034dea540f604791664f26091995cd0
peterennis <peterennis@yahoo.com>
2020-01-10 15:13:09 -0800

    feat(AD): get FSMO roles info

---
+++ PS_Examples/PS_GetFSMO.ps1
@@ -0,0 +1,6 @@
+Get-ADForest |`
+    Select-Object SchemaMaster, DomainNamingMaster |`
+    Format-List
+Get-ADDomain |`
+    Select-Object PDCEmulator, RIDMaster, InfrastructureMaster |`
+    Format-List
